2dbbc8d20d1f30b790f4a016598e1c83bc8d612a
SongYunSeop <dbstjq91@gmail.com>
2020-06-04 09:13:36 +0900

    🛠  fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const withSourceMaps = require('@zeit/next-source-maps');
+const withPWA = require('next-pwa');
 
-module.exports = withSourceMaps({
-  target: "serverless",
-  webpack(config, _options) {
-    return config;
-  }
-});
-
+module.exports = withSourceMaps(withPWA({
+    target: "serverless",
+    pwa: {
+        dest: 'public',
+    },
+    webpack(config, _options) {
+        return config;
+    }
+}));
